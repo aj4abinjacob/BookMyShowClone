1a3dab1b738a48a2ed6f11143ced6e259b847d3e
Abin Jacob <aj4abinjacob@gmail.com>
2025-04-23 08:15:23 +0530

    modified create movie function

--- Server/Controllers/movies.controllers.js
+++ Server/Controllers/movies.controllers.js
@@ -1,9 +1,22 @@
+const MovieModel = require("../Models/Movie.model");
+
 const getAllMovies = (req, res) => {
     return res.status(200).json({ message: "Get all movies" });
 }
 
-const createNewMovie = (req, res) => {
-    return res.status(201).json({ message: "Create new movie" });
+const createNewMovie = async (req, res) => {
+    try{
+        console.log("Movie data:", req.body);
+        const newMovie = MovieModel(req.body);
+        await newMovie.save();
+
+        return res.status(201).json({ message: "Movie created successfully", movie: newMovie });
+
+    }catch(err){
+        console.error("Error creating new movie:", err);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+
 }
 
 module.exports = {
