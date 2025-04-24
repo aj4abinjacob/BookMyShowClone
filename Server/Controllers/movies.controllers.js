dccdf65b157a179ee93142caa18b7a301a5d1345
Abin Jacob <aj4abinjacob@gmail.com>
2025-04-24 19:26:57 +0530

    Added update movie method

--- Server/Controllers/movies.controllers.js
+++ Server/Controllers/movies.controllers.js
@@ -1,14 +1,29 @@
 const MovieModel = require("../Models/Movie.model");
 
-const getAllMovies = (req, res) => {
-    return res.status(200).json({ message: "Get all movies" });
+const getAllMovies = async (req, res) => {
+    try{
+        const allMovies = await MovieModel.find();
+        if (!allMovies) {
+            return res.status(404).json({ message: "No movies found" });
+        }
+        console.log("All movies fetched:", allMovies);
+        return res.status(200).json({ message: "Movies fetched successfully", movies: allMovies });
+    }catch(err){
+        console.error("Error fetching movies:", err);
+        return res.status(500).json({ message: "Internal server error" });
+    }
 }
 
 const createNewMovie = async (req, res) => {
     try{
         console.log("Movie data:", req.body);
         const newMovie = MovieModel(req.body);
-        await newMovie.save();
+        const dbResponse = await newMovie.save();
+
+        if (!dbResponse) {
+            return res.status(500).json({ message: "Failed to create movie" });
+        }
+        console.log("New movie created:", dbResponse);
 
         return res.status(201).json({ message: "Movie created successfully", movie: newMovie });
 
@@ -19,7 +34,13 @@ const createNewMovie = async (req, res) => {
 
 }
 
+const updateMovieById = async (req, res) => {
+    console.log("Update movie data:", req.body);
+    return res.status(200).json({ message: "Movie updated successfully" });
+}
+
 module.exports = {
     getAllMovies,
-    createNewMovie
+    createNewMovie,
+    updateMovieById
 }
--- Server/Routes/movies.routes.js
+++ Server/Routes/movies.routes.js
@@ -1,8 +1,9 @@
-const { getAllMovies, createNewMovie } = require("../Controllers/movies.controllers");
+const { getAllMovies, createNewMovie, updateMovieById } = require("../Controllers/movies.controllers");
 const { verifyToken, verifyAdmin } = require("../Middlewares/auth.middlewares");
 
 module.exports = (app) => {
     app.get("/movies", [verifyToken], getAllMovies);
     app.post("/movies", [verifyToken, verifyAdmin], createNewMovie);
+    app.put("/movies/:id", [verifyToken, verifyAdmin], updateMovieById);
 
 }
