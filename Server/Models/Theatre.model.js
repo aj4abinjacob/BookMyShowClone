99ccd1076b31f851fb35130087d06ec057ce73f5
Abin Jacob <aj4abinjacob@gmail.com>
2025-04-24 20:47:20 +0530

    Added Theatre model

---
+++ Server/Models/Theatre.model.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const theatreSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    address: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    phone: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    location: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    isActive: {
+        type: Boolean,
+        default: false
+    },
+
+
+}, { timestamps: true });
+const TheatreModel = mongoose.model('Theatre', theatreSchema);
+module.exports = TheatreModel;
