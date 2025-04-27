7f23bbfe9bcd4c317f3625ef756e5ae9cfc235bd
Abin Jacob <aj4abinjacob@gmail.com>
2025-04-27 09:53:05 +0530

    Added search filtering

--- Client/book-my-show/src/pages/Home/Home.js
+++ Client/book-my-show/src/pages/Home/Home.js
@@ -11,6 +11,8 @@ function Home() {
   const [movies, setMovies] = useState([]);  // Initialize as empty array instead of null
   const [loading, setLoading] = useState(true);  // Add loading state
 
+  const [searchText, setSearchText] = useState("");
+
   const navigate = useNavigate();
 
   const fetchMovies = async () => {
@@ -28,6 +30,10 @@ function Home() {
   useEffect(() => {
     fetchMovies();
   }, []);
+
+  const onSearchInputChange = (e) => {
+    setSearchText(e.target.value);
+  }
   
   
   return (
@@ -35,7 +41,7 @@ function Home() {
       <NavBar/>
       <Row style={{justifyContent:"center",marginTop:"20px"}} className="d-flex justify-content-center w-100">
         <Col lg={{span:12}} xs={{span:24}} >
-        <Input placeholder="Type here to search for movies" />
+        <Input value={searchText} onChange={onSearchInputChange} placeholder="Type here to search for movies" />
         </Col>
       </Row>
       <Row gutter={[16, 24]} style={{ marginTop: "20px", marginLeft: "20px", marginRight: "20px", marginBottom: "20px" }}>
@@ -44,30 +50,32 @@ function Home() {
             Loading movies...
           </Col>
         ) : movies && movies.length > 0 ? (
-          movies.map((movie) => (
-            <Col
-              key={movie._id}
-              lg={{ span: 6 }}
-              xs={{ span: 12 }}
-              style={{ marginTop: "20px" }}
-            >
-              <div className="movie-card">
-                <img
-                  src={movie.poster}
-                  alt={movie.movieName}
-                  style={{ width: "100%", height: "300px", objectFit: "cover" }}
-                />
-                <h3 className="cursor-pointer" onClick={()=>{
-                  navigate(`/movie/${movie._id}`);
-                }}>{movie.movieName}</h3>
-                <p>{movie.description}</p>
-                <p>Genre: {movie.genre.join(", ")}</p>
-                <p>Duration: {movie.duration} minutes</p>
-                <p>Language: {movie.language}</p>
-                <p>Release Date: {new Date(movie.releaseDate).toLocaleDateString()}</p>
-              </div>
-            </Col>
-          ))
+          movies
+            .filter((movie) => movie.movieName.toLowerCase().includes(searchText.toLowerCase()))
+            .map((movie) => (
+              <Col
+                key={movie._id}
+                lg={{ span: 6 }}
+                xs={{ span: 12 }}
+                style={{ marginTop: "20px" }}
+              >
+                <div className="movie-card">
+                  <img
+                    src={movie.poster}
+                    alt={movie.movieName}
+                    style={{ width: "100%", height: "300px", objectFit: "cover" }}
+                  />
+                  <h3 className="cursor-pointer" onClick={() => {
+                    navigate(`/movie/${movie._id}`);
+                  }}>{movie.movieName}</h3>
+                  <p>{movie.description}</p>
+                  <p>Genre: {movie.genre.join(", ")}</p>
+                  <p>Duration: {movie.duration} minutes</p>
+                  <p>Language: {movie.language}</p>
+                  <p>Release Date: {new Date(movie.releaseDate).toLocaleDateString()}</p>
+                </div>
+              </Col>
+            ))
         ) : (
           <Col span={24} style={{ textAlign: 'center', marginTop: '50px' }}>
             No movies found.
