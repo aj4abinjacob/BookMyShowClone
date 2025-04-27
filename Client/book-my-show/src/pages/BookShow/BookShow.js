1237849e2ccb6c88c9485f3e3d9c363359a8ac34
Abin Jacob <aj4abinjacob@gmail.com>
2025-04-27 16:39:21 +0530

    Added BookShow component, template

--- Client/book-my-show/src/pages/BookShow/BookShow.js
+++ Client/book-my-show/src/pages/BookShow/BookShow.js
@@ -1,5 +1,14 @@
 
+import { useParams } from "react-router-dom";
+
 function BookShow() {
+    const params = useParams();
+
+    console.log("BookShow component mounted with params:", params);
+    const { movieId, showId } = params;
+    console.log("Movie ID:", movieId);      
+    console.log("Show ID:", showId);
+
   return (
     <div>
       <h1>Book Show</h1>
