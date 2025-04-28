48ee2e84351838fccbc3a0caf11ec3fda2217fc5
Abin Jacob <aj4abinjacob@gmail.com>
2025-04-28 23:25:53 +0530

    fixed issue with wrong imports

--- Server/src/Controllers/auth.controllers.js
+++ Server/src/Controllers/auth.controllers.js
@@ -1,4 +1,4 @@
-const User = require("../Models/User.model");
+const User = require("../Models/user.model");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const sendEmail = require("../Utils/NotificationUtils");
