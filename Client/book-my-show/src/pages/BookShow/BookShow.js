4fc0daf2a350b374d19685dc012b2a1d43aace8f
Abin Jacob <aj4abinjacob@gmail.com>
2025-04-28 20:12:27 +0530

    Added movie details

--- Client/book-my-show/src/pages/BookShow/BookShow.js
+++ Client/book-my-show/src/pages/BookShow/BookShow.js
@@ -4,6 +4,8 @@ import { useEffect } from "react";
 import { useState } from "react";
 import { getShowDetails } from "../../calls/shows";
 import Navbar from "../../Components/NavBar/NavBar";
+import { Card, Col, Row } from "antd";
+import { message } from "antd";
 
 function BookShow() {
     const params = useParams();
@@ -22,6 +24,10 @@ function BookShow() {
         console.error("Error fetching show data:", err);
       }
     }
+    useEffect(() => {
+      console.log("BookShow component mounted with params:", params);
+      fetchShowData();
+    }, [params.showId]);
 
     console.log("BookShow component mounted with params:", params);
     const { movieId, showId } = params;
@@ -31,10 +37,46 @@ function BookShow() {
     return (
       <>
         <Navbar />
-        <div>
-          <h1>Book Show</h1>
-          <p>Book your favorite shows here!</p>
-        </div>
+        {showDetails && (
+          <div>
+            <Row gutter={24}>
+              <Col span={24}>
+                <Card
+                  title={
+                    <div>
+                      <h1>{showDetails.movie.movieName}</h1>
+                      <p>
+                        Theatre: {showDetails.theatre.name}, {showDetails.theatre.address}
+                      </p>
+                    </div>
+                  }
+                  extra={
+                    <>
+                      <div>
+                        <h3>Show Time: {showDetails.time}</h3>
+                      </div>
+                      <h4>
+                        Ticket Price: {showDetails.ticketPrice}
+                      </h4>
+                      <h4>
+                        Total Seats: {showDetails.totalSeats}
+                      </h4>
+                      <h4>
+                        Available Seats: {showDetails.totalSeats - showDetails.bookedSeats.length}
+                      </h4>
+                    </>
+                  }
+                  style={{ width: "100%" }}
+                >
+                  <p>{showDetails.movie.description}</p>
+                  <p>Duration: {showDetails.movie.duration} minutes</p>
+                  <p>Genre: {showDetails.movie.genre.join(", ")}</p>
+                  <p>Language: {showDetails.movie.language}</p>
+                </Card>
+              </Col>
+            </Row>
+          </div>
+        )}
       </>
     );
   }
