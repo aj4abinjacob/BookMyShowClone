4ba693e71ff31b8e9682d297330e237ecbd98f0d
Abin Jacob <aj4abinjacob@gmail.com>
2025-04-30 20:57:33 +0530

    Added code to reroute if the user tries to access home without login

--- Client/book-my-show/src/App.js
+++ Client/book-my-show/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {BrowserRouter, Route, Routes, Navigate} from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
@@ -8,13 +8,24 @@ import BookShow from './pages/BookShow/BookShow';
 import ForgetPassword from './pages/Forget/Forget';
 import ResetPassword from './pages/Reset/Reset';
 
+const ProtectedRoute = ({ children }) => {
+  const token = localStorage.getItem('token');
+  if (!token) {
+    return <Navigate to="/login" replace />;
+  }
+  return children;
+};
 
 function App() {
   return (
     <div>
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Home/>} />
+      <Route path="/" element={
+        <ProtectedRoute>
+          <Home/>
+        </ProtectedRoute>
+      } />
       <Route path="/login" element={<Login/>} />
       <Route path="/register" element={<Register/>} />
       <Route path="/forget" element={<ForgetPassword/>} />
@@ -23,7 +34,6 @@ function App() {
       <Route path="/movie/:movieId/book-show/:showId" element={<BookShow/>} />
     </Routes>
     </BrowserRouter>
-
     </div>
   );
 }
